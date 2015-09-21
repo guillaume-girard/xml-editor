2212a5cf2f72a3f89c9b4eb91529935ca9e95ec3
guillaume-girard <dev.guillaume.girard@gmail.com>
2015-09-21 15:50:13 +0200

    Configuration template now describe a library

--- cfg/config.js
+++ cfg/config.js
@@ -2,82 +2,71 @@
 
 var XMLEditor = XMLEditor || {};
 XMLEditor.Config = {
-    _attributes_: {
-        id: {  // id pour le graph
-            xmlName: "Id",  // nom affiché + nom de l'attr dans le xml
-            type: "input",  //type d'attributs (input|boolean|select|textarea)
-            required: true, // true si l'attributs est obligatoire
-            defaultvalue: null   // valeur par défaut si besoin
-        },
-        title: {  // id pour le graph
-            xmlName: "Title",  // nom affiché + nom de l'attr dans le xml
-            type: "input",  //type d'attributs (input|boolean|select|textarea)
-            required: true, // true si l'attributs est obligatoire
-            defaultvalue: null   // valeur par défaut si besoin
-        },
-        description: {
-            xmlName: "Description",
-            type: "textarea",
-            required: false,
-            defaultvalue: null
+    _attributes_: {    // attributes of the current tag
+        adress: {          // key for the parser
+            xmlName: "Adress",   // field label and XML key
+            type: "input",       // type of the attribute (input|boolean|select|textarea)
+            required: true,      // true if the attribute is required
+            defaultvalue: null   // default value if there is one
         }
     },
     _tags_: {
-        precondition: {
-            xmlName: "Precondition",
+        books: {
+            xmlName: "Books",
             multiple: false,
-            required: false,
+            required: true,
             _attributes_: {},
-            text: "default_text_precondition",
-            _tags_: {}
-        },
-        postcondition: {
-            xmlName: "Postcondition",
-            multiple: false,
-            required: false,
-            _attributes_: {},
-            text: true,
-            _tags_: {}
-        },
-        attributes: {
-            xmlName: "Attributes",
-            multiple: false,
-            required: false,
-            _attributes_: {},
-            text: false,
-            _tags_: {
-                attribute: {
-                    xmlName: "Attribute",
-                    multiple: true,
-                    required: true,
+            text: false,     // text node (true | false | default value)
+            _tags_: {        // children tags of the current tag
+                book: {
+                    xmlName: "Book",
+                    multiple: true,    // true if there can be many of this tag
+                    required: true,    // true if we need at least one of this tag
                     _attributes_: {
-                        name: {
-                            xmlName: "name",  
-                            type: "select",  
-                            required: false, 
-                            defaultvalue: null, 
-                            possibleValues: ["actor", "nexus", "user", "xCase"]
+                        title: { 
+                            xmlName: "Title",  
+                            type: "input",  
+                            required: true, 
+                            defaultvalue: null   
+                        },
+                        publicationdate: {
+                            xmlName: "PublicationDate",
+                            type: "input",
+                            required: false,
+                            defaultvalue: null
                         }
                     },
                     text: false,
-                    _tags_: {}    // pas de balises filles
-                }
-            }
-        },
-        params: {
-            xmlName: "Params",
-            multiple: false,
-            required: false,
-            _attributes_: {},
-            text: false,
-            _tags_: {
-                param: {
-                    xmlName: "Param",
-                    multiple: false,
-                    required: true,
-                    _attributes_: {},
-                    text: false,
-                    _tags_: {}    // pas de balises filles
+                    _tags_: { 
+                        author: {
+                            xmlName: "Author",
+                            multiple: false,
+                            required: false,
+                            _attributes_: {
+                                name: {
+                                    xmlName: "Name", 
+                                    type: "input",
+                                    required: true,
+                                    defaultvalue: null
+                                },
+                                birthdate: {
+                                    xmlName: "Birthdate",
+                                    type: "input",
+                                    required: false,
+                                    defaultvalue: null
+                                },
+                                nationality: {
+                                    xmlName: "Nationality",
+                                    type: "select",
+                                    required: false,
+                                    defaultvalue: null,
+                                    possibleValues: ["EN", "US", "EI", "FR", "GE", "IT", "Other"]
+                                }
+                            }, 
+                            text: false,
+                            _tags_: {}
+                        }
+                    }
                 }
             }
         }
